99193ffba553a526e9d87682c852b50e386129c0
nickoboco <nickolas.selhorst@gmail.com>
2025-08-31 22:17:16 -0300

    feat: configuração segura para GitHub Pages

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyA-7Du4OFJ75KYUw-o0yJKAnpREwjJb69g",
-  authDomain: "internetniuncamais.firebaseapp.com",
-  projectId: "internetniuncamais",
-  storageBucket: "internetniuncamais.firebasestorage.app",
-  messagingSenderId: "503832825549",
-  appId: "1:503832825549:web:fe09c58e3f4b101faee384"
-};
